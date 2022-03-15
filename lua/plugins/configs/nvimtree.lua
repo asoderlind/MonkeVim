c3d5caaf33226138b8c9ae5e08facbef8f8408a8
Henrik Hansson <hinkik@gmail.com>
2022-03-15 23:57:15 +0100

    move setting to adhere to new version

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -11,7 +11,6 @@ vim.o.termguicolors = true
 g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
 g.nvim_tree_highlight_opened_files = 0
 g.nvim_tree_indent_markers = 1
-g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
 g.nvim_tree_root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" }
 
 g.nvim_tree_show_icons = {
@@ -51,6 +50,7 @@ nvimtree.setup {
    open_on_tab         = false,
    hijack_cursor       = false,
    update_cwd          = false,
+   quit_on_open        = false,
    update_to_buf_dir   = {
      enable = true,
      auto_open = true,
