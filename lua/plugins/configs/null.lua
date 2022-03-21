50835030201e84f279b98cb55d2abe5d1f8e1b41
Axel <axesode+github@gmail.com>
2022-03-21 17:54:29 +0100

    auto-format with clang

--- lua/plugins/configs/null.lua
+++ lua/plugins/configs/null.lua
@@ -3,6 +3,7 @@ local _, null_ls = pcall(require, "null-ls")
 local sources = {
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.eslint_d,
+    null_ls.builtins.formatting.clang_format,
 }
 
 null_ls.setup({
