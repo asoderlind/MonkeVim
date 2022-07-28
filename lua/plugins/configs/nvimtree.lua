753a62cb0d08800b19c43f0ddc859516621f5899
Axel <axesode+github@gmail.com>
2022-07-28 23:34:44 +0900

    moved options

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -8,40 +8,6 @@ local g = vim.g
 
 vim.o.termguicolors = true
 
-g.nvim_tree_respect_buf_cwd = 1
-g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
-g.nvim_tree_highlight_opened_files = 0
-g.nvim_tree_indent_markers = 1
-g.nvim_tree_root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" }
-
-g.nvim_tree_show_icons = {
-   folders = 1,
-   files = 1,
-   git = 1,
-}
-
-g.nvim_tree_icons = {
-   default = "",
-   symlink = "",
-   git = {
-      deleted = "",
-      ignored = "◌",
-      renamed = "➜",
-      staged = "✓",
-      unmerged = "",
-      unstaged = "✗",
-      untracked = "★",
-   },
-   folder = {
-      default = "",
-      empty = "", -- 
-      empty_open = "",
-      open = "",
-      symlink = "",
-      symlink_open = "",
-   },
-}
-
 nvimtree.setup {
    disable_netrw       = true,
    hijack_netrw        = true,
@@ -49,6 +15,7 @@ nvimtree.setup {
    ignore_ft_on_setup  = {},
    open_on_tab         = false,
    hijack_cursor       = false,
+   respect_buf_cwd     = true,
    update_cwd          = false,
    update_to_buf_dir   = {
      enable = true,
@@ -99,4 +66,41 @@ nvimtree.setup {
      cmd = "trash",
      require_confirm = true
   },
+  renderer = {
+    add_trailing = false,
+    highlight_opened_files = "all",
+    indent_markers = {
+      enable = true
+    },
+    root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" },
+    icons = {
+      show = {
+        file = true,
+        folder = true,
+        folder_arrow = true,
+        git = true,
+      },
+      glyphs = {
+         default = "",
+         symlink = "",
+         git = {
+            deleted = "",
+            ignored = "◌",
+            renamed = "➜",
+            staged = "✓",
+            unmerged = "",
+            unstaged = "✗",
+            untracked = "★",
+         },
+         folder = {
+            default = "",
+            empty = "", -- 
+            empty_open = "",
+            open = "",
+            symlink = "",
+            symlink_open = "",
+         },
+      }
+    }
+  }
 }
